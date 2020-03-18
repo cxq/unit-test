53045b4ba0bf59617808af09456b43b91914d3ac
Xiu Qiang Chu <XiuQiang.CHU@loreal.com>
2020-03-18 17:05:30 +0100

    Add test structure

---
+++ test/test.test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+describe('Test', function() {
+  describe('#methodOne', function() {
+    it('should return true when the value is true', function() {
+        expect(true).to.be.true;
+    });
+  });
+});
